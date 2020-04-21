4906516b576354c4fa55ecef5b9a12e9de79f869
EMIDEC-challenge <zhihao.chen.paul@gmail.com>
2020-04-21 14:47:40 +0200

    2d Dice added; README redid

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Evaluation-metrics
-Evaluation of cardiac MRI predictions with given metrics
+This is the evaluation code for cardiac MRI predictions with given metrics.
+
+
+# How to use
+Prepare your prediction masks and the groud truth masks as the following instructions. Then in the "main.py" define your mask paths by the variables "pathPrediction" and "pathGT", and specify the class
+you want to evaluate by the varibale "label". You can save your result on .csv by uncommenting the last section.
+
+You may need to install some dependencies by:
+
+	pip install SimpleITK
+	pip install scipy
+
+
+# Metrics
+For the myocardium, the Dice, the difference of the volumes, and the Hausdorff distance will be calculated between the grand truth and the prediction to evaluate the prediction masks' performance.
+
+For the scar tissues including the myocardiul infarction and the No-Reflow, the Dice, the difference of the volumes, and the difference rate of the volumes will be calculated between the grand truth and the prediction.
+
+The Dice is calculated firstly on each slice (2D), then the Dice of the case is the average of all the slices. 
+
+The difference of the volumes between the GT and the prediction is the absolute difference between the prediction volume and the GT volume. The voxel spacing is taken into account and the unit is mm³.
+
+The difference rate of the volumes between the GT and the prediction is calculated as the (difference of the scar tissu volume)/(myocardium volume). This metric can describe more objectively the scar tissu prediction's quality.
+
+
+#  Files orgnization
+The default file orgnization should be similar to the dataset.
+You should have 2 folders to put your predictions and the corresponding GTs. Both of them should have the same orgnization.
+For example, 
+root - GT - Case1 - Contours - Case1.nii.gz
+	 |	  |-Case2 - Contours - Case2.nii.gz
+	 |	  |-CaseX - Contours - CaseX.nii.gz	
+	 |
+	 |-prediction- Case1 - Contours - Case1.nii.gz
+	 	        |- Case2 - Contours - Case2.nii.gz
+	 	  	    |- CaseX - Contours - CaseX.nii.gz	
+
+#  Mask file format and encoding
+This code loads the prediction and the GT mask from nifti files by default. You should take the contour nifti files from the dataset as the GT. Your prediction mask should have the same encoding as the mask in the dataset, that is: the mask should be encoded as catagorical labels from 0 to 4. The label and the class are {"background":0 ,"cavity":1, "normal_myocardium":2, "infarction":3, "NoReflow":4}. Notice that to evaluate the prediction of the myocardium, the mask labels should be the ensemble of 2,3 and 4.
--- main.py
+++ main.py
@@ -6,19 +6,11 @@ Created on Thu Apr  9 20:36:27 2020
 @author: chen
 """
 import metrics
-
 import numpy as np
 import os
 import shutil
 import SimpleITK as sitk
 import scipy.ndimage as ndimage
-
-###**************Load predicted and GT mask as a volume***********************
-
-#path of your prediction and GT floder
-#The prediction mask and GT mask should have a same or similar name. 
-pathPrediction="prediction"
-pathGT="GT"
 
 dice=[]
 HD=[]
@@ -30,13 +22,18 @@ volumePrediction=[]
 #*********************************************
 #choose the tissue class you want to calculate here
 #label=("Myocardium", "Infarction", "No Reflow")
-label="Infarction"
+label="No Reflow"
+#path of your prediction and GT floder
+pathPrediction="prediction"
+pathGT="GT"
 #*********************************************
 
-for filePrediction in os.listdir(pathPrediction):
+
+for filePrediction in sorted(os.listdir(pathPrediction)):
     #  load prediction mask as a nifiti, you can use nib.load as well for nifti
     prediction = sitk.ReadImage(os.path.join(pathPrediction, filePrediction, 'Contours', filePrediction+'.nii.gz'), sitk.sitkInt16) 
-    #  the prediction mask array should be one hot format
+    #  the prediction mask array should be encoded as the categorical value from 0 to 4. 
+    #  the correspendnence of the Value-Class should be the same as the GT mask 
     predArray = sitk.GetArrayFromImage(prediction)  # convert into numpy array
 
     # load GT mask. 
@@ -44,7 +41,6 @@ for filePrediction in os.listdir(pathPrediction):
     GT = sitk.ReadImage(os.path.join(pathGT, filePrediction, 'Contours', filePrediction+'.nii.gz'), sitk.sitkInt16) 
     GTArray = sitk.GetArrayFromImage(GT)
     spacing=GT.GetSpacing()
-    
     #  get the one hot GT mask of the indexed class
     if label=="Myocardium": #The Myocardium includes both the normal myocardium and scar tissue
         aGTArray = (GTArray==2) + (GTArray==3) + (GTArray==4)
@@ -54,12 +50,12 @@ for filePrediction in os.listdir(pathPrediction):
     elif label=="Infarction":
         aGTArray = GTArray==3
         aPredArray = predArray==3
-        #aPredArray[1:3] = np.zeros_like(aPredArray[1:3])
+        #aPredArray[3:4] = np.zeros_like(aPredArray[3:4])
         
     elif label=="No Reflow":
         aGTArray = GTArray==4
         aPredArray = predArray==4
-        #aPredArray[1:3] = np.zeros_like(aPredArray[1:3])
+        #aPredArray[1:7] = np.zeros_like(aPredArray[1:7])
     else:
         raise NameError('Unknown class name')
     ###*****************metrics calculation*****************
@@ -67,28 +63,30 @@ for filePrediction in os.listdir(pathPrediction):
     dice.append(metrics.dc(aPredArray, aGTArray))
     aVolumePred=metrics.volume(aPredArray, spacing)
     aVolumeGT=metrics.volume(aGTArray, spacing)
-    volumePrediction.append(aVolumePred)
-    volumeDifference.append(abs(aVolumePred-aVolumeGT))
-    print(filePrediction, aVolumePred)
-    ###****************metric for myocardium***********
+    volumePrediction.append(round(aVolumePred, 2))
+    volumeDifference.append(round(abs(aVolumePred-aVolumeGT),2))
+    ###****************particular metric for myocardium***********
     if label=="Myocardium":
-        HD.append(metrics.hd(predArray, GTArray))
+        HD.append(metrics.hd(aPredArray, aGTArray, spacing))
+
         
-    ###****************metric for scar tissues***********
+    ###****************particular metric for scar tissues***********
     else:
         aVolumeMyo=metrics.volume((GTArray==2) + (GTArray==3) + (GTArray==4), spacing)
         volumeDifferenceRate.append(abs(aVolumePred-aVolumeGT)/aVolumeMyo)
         
 
 
-    
-
 avgDice = float(sum(dice))/len(dice)
+print("Average Dice: ", round(avgDice, 4))
 avgVD= float(sum(volumeDifference))/len(volumeDifference)
+print("Average volume difference: ", round(avgVD, 2), "mm\N{SUPERSCRIPT THREE}")
 if label=="Myocardium":
     avgHd= float(sum(HD))/len(HD)
+    print("Average Hausdorff distance: ", round(avgHd, 2), "mm")
 else:
     avgVDR= float(sum(volumeDifferenceRate))/len(volumeDifferenceRate)
+    print("Average volume difference rate: ", round(avgVDR, 4))
 
 '''
 #comment the csv you don't want to save
